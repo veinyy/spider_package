49593ba7e732fea42d5d02590ea9c1674f964cb6
zhangliang <veinyy@163.com>
2015-09-14 00:52:46 +0800

    add modify_filename function

--- scrapy package/meitu/modify_filename.py
+++ scrapy package/meitu/modify_filename.py
@@ -1,2 +1,14 @@
 import os
-D:\test_pic\full
+import sys
+files = os.listdir('')
+
+path=""
+flag=True
+for i ,name in enumerate(files):
+    new_name = "%s.jpg" % str(i)
+    old_name = "%s" % str(name)
+    if flag:
+        os.chdir(path)
+        flag=False
+    os.rename(old_name,new_name)
+
